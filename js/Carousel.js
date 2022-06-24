6bb8b92408ef2962a5be35b9aad7eb8aa4b331ea
Kribas Rimal <32868850+Kribas@users.noreply.github.com>
2022-06-24 20:38:13 +0545

    Add files via upload
    made changes

--- js/Carousel.js
+++ js/Carousel.js
@@ -1,5 +1,37 @@
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
+    $("#testimonial-carousel").owlCarousel({
+        loop: true,
+        items: 4,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        margin: 20,
+        responsiveClass: true,
+        responsive:{
+
+            0:{
+    
+                items:1
+    
+            },
+    
+            600:{
+    
+                items:1
+    
+            },
+    
+            1000:{
+    
+                items:4,
+    
+                loop:true
+    
+            }
+    
+        }
+    });
+
+    $("#our-partners-carousel").owlCarousel({
         loop: true,
         items: 4,
         autoplay: true,
